9990356a9806891055e755abbb9849c0202d06f0
tzfunc <tzf_index@protonmail.com>
2023-03-26 04:01:15 +0800

    数据库配置

--- Django_blog/settings.py
+++ Django_blog/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "Django_blog.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
+        "DIRS": []
         ,
         "APP_DIRS": True,
         "OPTIONS": {
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "Django_blog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django_blog",
+        "USER": 'root',
+        "PASSWORD": '123',
+        "HOST": 'localhost',
+        "PORT": '3306',
     }
 }
 
